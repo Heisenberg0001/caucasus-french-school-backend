f1a7161f9b1e83fa0fe612b781faeca5e2ae84d9
George Chlaidze <george.chlaidze@gmail.com>
2022-10-24 23:11:16 +0400

    Initial Commit

---
+++ schemas/news.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'news',
+    type: 'document',
+    title: 'News',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title'
+        },
+        {
+            name: 'description',
+            type: 'string',
+            title: 'Description'
+        }
+    ]
+}
